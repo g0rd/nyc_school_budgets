1c975e2b5d7d2dc25317d8b87cf3cf0746ffabbb
g0rd <r.devashrayee@gmail.com>
2014-09-21 00:40:59 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -66,6 +66,7 @@ def scrape(schoolcode):
     ell_budget = tree.cssselect('span#doecontrol_bottomcentercontainer_School_Budget_Overview_lblELLSubTotal_C04')
     print ell_budget[0].text_content().encode("utf-8").strip()
     school_str = school_name[0].text_content().encode("utf-8").split().pop(0)
+    pprint(school_str)
     school= str.join(school_str)
     budget = ell_budget[0].text_content().encode("utf-8").strip()
     output = {"school_id": schoolcode, "school_name":school, "budget": budget }
