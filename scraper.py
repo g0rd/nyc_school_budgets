348fe00f6a12747b556328b4ad4f58dc95549ca3
g0rd <r.devashrayee@gmail.com>
2014-09-26 16:23:20 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -145,7 +145,7 @@ def get_school_ids():
             yield cell.value[2:]
 
 
-def main(reset=False, first_code=None):
+def main(reset=False, first_code='M015'):
     import time
     start = time.time()
     codes = list(get_school_ids())
